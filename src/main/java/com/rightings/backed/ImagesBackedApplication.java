67e39bdde174632cf1d15fc721af2c642611f57b
righting1 <2076935979@qq.com>
2025-03-23 18:43:00 +0800

    启动类

---
+++ src/main/java/com/rightings/backed/ImagesBackedApplication.java
@@ -0,0 +1,11 @@
+package com.rightings.backed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagesBackedApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ImagesBackedApplication.class, args);
+    }
+}
