1ecb304b8f79c2dc2ff49ee708ab3b665e094c2b
righting1 <2076935979@qq.com>
2025-03-24 10:18:25 +0800

    update2

--- src/main/java/com/rightings/backed/demo/HelloController.java
+++ src/main/java/com/rightings/backed/demo/HelloController.java
@@ -1,18 +1,30 @@
 package com.rightings.backed.demo;
 
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RequestMapping("/test")
 @RestController
 public class HelloController {
 
 
+    @Resource
+    private RestTemplate redisStep;
 
     @GetMapping("/hello")
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello1")
+    public String hello1() {
+
+        return "hello";
+    }
+
 }
