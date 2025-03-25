701fbf93eabe00a9f03a0b7c90b4a744d3e426b6
righting1 <2076935979@qq.com>
2025-03-25 23:37:23 +0800

    commit2

--- src/main/java/com/rightings/backed/ImagesBackedApplication.java
+++ src/main/java/com/rightings/backed/ImagesBackedApplication.java
@@ -1,9 +1,16 @@
 package com.rightings.backed;
 
+import org.apache.shardingsphere.spring.boot.ShardingSphereAutoConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ShardingSphereAutoConfiguration.class})
+@EnableAsync
+@MapperScan("com.rightings.backed.mapper")
+@EnableAspectJAutoProxy(exposeProxy = true)
 public class ImagesBackedApplication {
     public static void main(String[] args) {
         SpringApplication.run(ImagesBackedApplication.class, args);
--- src/main/java/com/rightings/backed/demo/HelloController.java
+++ src/main/java/com/rightings/backed/demo/HelloController.java
@@ -4,17 +4,12 @@ package com.rightings.backed.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 
 @RequestMapping("/test")
 @RestController
 public class HelloController {
 
-
-    @Resource
-    private RestTemplate redisStep;
 
     @GetMapping("/hello")
     public String hello() {
